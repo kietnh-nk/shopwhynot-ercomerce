6319281db39031dfa31959591b8b392ba382e8ef
thangnv05 <thangnvpn00120@gmail.com>
2025-08-18 22:45:54 +0700

    Create model
    thêm những phần của model

---
+++ app/Models/UserVoucher.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserVoucher extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'promotion_id',
+        'isUsed',
+    ];
+    public $timestamps = false;
+    // Define relationships if needed
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function promotion()
+    {
+        return $this->belongsTo(Promotion::class);
+    }
+}
