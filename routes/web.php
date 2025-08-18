f65e74f125070d59e6e96d35d9464d3186ece8e4
dao115 <daopxpn00029@gmail.com>
2025-08-19 06:28:53 +0700

    quản lý bài viết

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    Route::group(['prefix' => 'post'], function () {
+    Route::get('index', [PostController::class, 'index'])->name('post.index');
+    Route::get('create', [PostController::class, 'create'])->name('post.create');
+    Route::post('store', [PostController::class, 'store'])->name('post.store');
+    Route::get('update/{slug}', [PostController::class, 'update'])->name('post.update');
+    Route::post('edit/{slug}', [PostController::class, 'edit'])->name('post.edit');
+    Route::get('delete/{id}', [PostController::class, 'delete'])->name('post.delete');
+    Route::delete('destroy/{id}', [PostController::class, 'destroy'])->name('post.destroy');
 });
+
