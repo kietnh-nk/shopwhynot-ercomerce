5a42dca92d0c0268597e078cd16e97172ba31138
thangnv05 <thangnvpn00120@gmail.com>
2025-08-22 21:52:14 +0700

    add Attribute
    thêm logic và tính năng của Attribute

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@ use App\Http\Controllers\Backend\UserController;
 use App\Http\Controllers\Backend\PostController;
 use App\Http\Controllers\Backend\RevenueController;
 use App\Http\Controllers\Backend\AttributeCatalogueController;
+use App\Http\Controllers\Backend\AttributeController;
+use App\Http\Controllers\Ajax\AttributeController as AjaxAttributeController;
 
 
 use Illuminate\Support\Facades\Route;
 
 
+// AJAX
+//SEARCH SUGGESTION
+Route::get('/ajax/search/suggestion', [AjaxSearchController::class, 'suggestion'])->name('ajax.search.suggestions');
+// ATTRIBUTE
+Route::get('/ajax/attribute/getAttribute', [AjaxAttributeController::class, 'getAttribute'])->name('ajax.attribute.getAttribute');
+Route::get('/ajax/attribute/loadAttribute', [AjaxAttributeController::class, 'loadAttribute'])->name('ajax.attribute.loadAttribute');
 // WEB ROUTES
 Route::get('/', [AuthController::class, 'index'])->name('home');
 // AUTH
@@ -107,6 +115,16 @@ Route::middleware(['auth', 'admin'])->group(function () {
         Route::post('edit/{slug}', [AttributeCatalogueController::class, 'edit'])->name('attribute.catalogue.edit');
         Route::get('delete/{id}', [AttributeCatalogueController::class, 'delete'])->where(['id' => '[0-9]+'])->name('attribute.catalogue.delete');
         Route::delete('destroy/{id}', [AttributeCatalogueController::class, 'destroy'])->where(['id' => '[0-9]+'])->name('attribute.catalogue.destroy');
+    });
+        // attribute
+    Route::group(['prefix' => 'attribute'], function () {
+        Route::get('index', [AttributeController::class, 'index'])->name('attribute.index');
+        Route::get('create', [AttributeController::class, 'create'])->name('attribute.create');
+        Route::post('store', [AttributeController::class, 'store'])->name('attribute.store');
+        Route::get('update/{slug}', [AttributeController::class, 'update'])->name('attribute.update');
+        Route::post('edit/{slug}', [AttributeController::class, 'edit'])->name('attribute.edit');
+        Route::get('delete/{id}', [AttributeController::class, 'delete'])->where(['id' => '[0-9]+'])->name('attribute.delete');
+        Route::delete('destroy/{id}', [AttributeController::class, 'destroy'])->where(['id' => '[0-9]+'])->name('attribute.destroy');
     });
     //Doanh thu
     Route::get('/admin/revenue', [RevenueController::class, 'index'])->name('revenue.index');
