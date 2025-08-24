353b634af0971501ca3ba4376cf37af713179340
Nguyễn Hữu Kiệt <kietnhpn00090@gmail.com>
2025-08-24 23:02:32 +0700

    feat(voucher and banner promotion)

---
+++ app/Http/Controllers/Backend/BrandController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Services\BrandService;
+use App\Http\Controllers\Controller;
+use App\Repositories\BrandRepository;
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Requests\UpdateBrandRequest;
+
+class BrandController extends Controller
+{
+    protected $brandService;
+    protected $brandRepository;
+    public function __construct(
+        BrandService $brandService,
+        BrandRepository $brandRepository,
+    ) {
+        $this->brandService = $brandService;
+        $this->brandRepository = $brandRepository;
+    }
+
+    public function index(Request $request)
+    {
+        $brands = $this->brandService->paginate($request);
+        $template = 'backend.brand.index';
+        return view('backend.dashboard.layout', compact(
+            'template',
+            'brands',
+        ));
+    }
+    public function create()
+    {
+        $brands = $this->brandService->all();
+        $template = 'backend.brand.create';
+        return view('backend.dashboard.layout', compact(
+            'template',
+            'brands',
+        ));
+    }
+    public function store(StoreBrandRequest $request)
+    {
+        // gọi tới service với phương thức create 
+        $result = $this->brandService->create($request);
+        if ($result) {
+            flash()->success('Thêm mới thành công');
+            return redirect()->route('brand.index');
+        } else {
+            flash()->error('Thất bại. Đã có lỗi xảy ra vui lòng thử lại!');
+            return redirect()->back();
+        }
+    }
+    public function update($slug)
+    {
+        $brand = $this->brandRepository->findBySlug($slug);
+        $template = 'backend.brand.update';
+        return view('backend.dashboard.layout', compact(
+            'template',
+            'brand',
+        ));
+    }
+    public function edit($slug, UpdateBrandRequest $request)
+    {
+        $result = $this->brandService->update($slug, $request);
+        if ($result) {
+            flash()->success('cập nhật thành công');
+            return redirect()->route('brand.index');
+        } else {
+            flash()->error('Thất bại. Đã có lỗi xảy ra vui lòng thử lại!');
+            return redirect()->back();
+        }
+    }
+    public function delete($id = 0)
+    {
+        $brand = $this->brandRepository->findById($id);
+        $template = 'backend.brand.delete';
+        return view('backend.dashboard.layout', compact(
+            'template',
+            'brand',
+        ));
+    }
+    public function destroy($id = 0)
+    {
+        if ($_POST['submit'] == 'cancel') {
+            flash()->warning('Nhóm thương hiệu chưa được xóa!');
+            return redirect()->route('brand.index');
+        } else {
+            $result = $this->brandService->destroy($id);
+            if ($result) {
+                flash()->success('Thương hiệu đã được xóa thành công!');
+                return redirect()->route('brand.index');
+            } else {
+                flash()->error('Có lỗi khi xóa, vui lòng thử lại!');
+                return redirect()->back();
+            }
+        }
+    }
+
+    public function destroyMultiple(Request $request)
+    {
+        // Lấy giá trị từ input ẩn đã truyền mảng ID
+        $listId = $request->input('array_id');
+
+        // Kiểm tra nếu có ID, và tách chuỗi thành mảng
+        if ($listId) {
+            $arrayId = explode(',', $listId);
+            //dd($arrayId);
+            $this->brandService->destroyBulk($arrayId);
+            flash()->success('Xóa thành công các bản ghi.');
+        } else {
+            flash()->warning('Không có bản ghi nào được chọn để xóa.');
+        }
+
+        return redirect()->route('brand.index');
+    }
+}
